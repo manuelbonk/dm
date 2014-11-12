99db20e37123c1aaa1b3a081687bad5ec1107390
Manuel Bonk <manuel@bonkii.com>
2014-11-12 12:15:49 +0100

    EOF is now also encoded

--- lzw.py
+++ lzw.py
@@ -44,6 +44,7 @@ def encode(toEncode,init_dictionary,new_entries_counter):
 
         print("neuer puffer: <%s>\n"%puffer)
         toEncode=toEncode[1:]
+    encoded.append(26)
     printDict(dictionary)
     print("\nmessage: <%s>\nLZW encoded message: <%s>\nbinary LZW encoded message: <%4s>\n" %(message,encoded,['{0:04b}'.format(x) for x in encoded]))
     # returns LZW encoded message in binary code
@@ -64,17 +65,17 @@ def convertToASCII(toConvert):
 
 # Anfangsalphabet darf nicht leer sein. (Keine Ahnung, ob das allgemein für LZW gilt oder nur für meinen Code)
 #encode("bananenanbau",genASCIIdict(),256)
-#encode("ballaballa",genASCIIdict(),256)
+encode("ballaballa",genASCIIdict(),256)
 
 # Aufgabe 1 a) und 1 c)
-print("Aufgabe 1 a) und 1 c):\n")
-encode("0012230331130121",{'0':0,'1':1,'2':2,'3':3},4)
+#print("Aufgabe 1 a) und 1 c):\n")
+#encode("0012230331130121",{'0':0,'1':1,'2':2,'3':3},4)
 
 
 # Aufgabe 2 a)
-task_2a=encode("bobobobowebewe",genASCIIdict(),256)
-print("Aufgabe 2a):\n",task_2a,"\n\n")
+#task_2a=encode("bobobobowebewe",genASCIIdict(),256)
+#print("Aufgabe 2a):\n",task_2a,"\n\n")
 # Aufgabe 2 b)
-print("Aufgabe 2b):")
-task_2b=convertToASCII("bobobobowebewe")
-print("\nplain (binary) ASCII encoded message: <%s>\n(binary) LZW encoded message: <%s>\nreduction: <%s>"%(task_2b,task_2a,len(task_2a)*9/(len(task_2b)*8)))
+#print("Aufgabe 2b):")
+#task_2b=convertToASCII("bobobobowebewe")
+#print("\nplain (binary) ASCII encoded message: <%s>\n(binary) LZW encoded message: <%s>\nreduction: <%s>"%(task_2b,task_2a,len(task_2a)*9/(len(task_2b)*8)))
